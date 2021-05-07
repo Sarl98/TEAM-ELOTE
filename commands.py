e0db56682ab292fbe0e37022872721864c20691a
jorgemerlo <jorgemerlosantos@gmail.com>
2021-05-07 18:39:28 -0500

    act12

--- commands.py
+++ commands.py
@@ -1,6 +1,7 @@
 import argparse
 import nltk
 import ast
+import os
 from main import *
 
 # commands genera los comandos y lee los argumentos
@@ -14,6 +15,8 @@ def commands():
     parser=argparse.ArgumentParser(description="Tokenizar o indexar")
     parser.add_argument("-tok",help="tokenizar",dest="tok",action='store_false',required=False)
     parser.add_argument("-ind",help="indexar",dest="ind",action='store_false',required=False)
+    parser.add_argument('-search',action="extend", metavar='name', type=str, nargs='+',
+                    help='busca archivos con esta palabra',required=False)
     parser.set_defaults(func=run)
     args=parser.parse_args()
     args.func(args)
@@ -29,6 +32,24 @@ def commands():
 
 def run(args):
     
+    if(args.search):
+        fileTimeOpen = time.time()
+        foundOne = False
+        for file in os.listdir("wordlists"):
+            dictonary = open("wordlists/" + str(file), "r").read()
+            for word in args.search:
+                if(word in dictonary):
+                    foundOne = True
+                    print("['" +word + "'] encontrado en el archivo: ['" + file + "']")
+        fileTimeClose = time.time()
+        timeCountString = ("Se tardo en encontrar: " + str(round(fileTimeClose - fileTimeOpen, 4)) + " segundos\n")
+        print(timeCountString)  
+        if(not foundOne):
+            print("['" +word + "'] no se encontro en ningun archivo")
+            timeCountString = timeCountString + "no se encontro ningun archivo con el token: " + word
+          
+        createFile("tiempos-de-busqueda-wordlists.txt", timeCountString, True)
+
     if(args.tok == False):
         print("Creando Tokens")
         allTokenizedWordsCountPerFile = {}
